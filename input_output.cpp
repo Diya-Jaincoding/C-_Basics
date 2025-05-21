8e4fd333377501b57b5b477bef5514ac342b6e0f
Diya-Jaincoding <diyajain220362@acropolis.in>
2025-05-21 21:28:57 +0530

    Day 1: Added input and output basics in C++

---
+++ input_output.cpp
@@ -0,0 +1,11 @@
+// Day 1: Input and Output in C++
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter two numbers:";
+    cin>> a>>b;
+    cout<< "Sum is:" << a+b<< endl;
+    return 0;
+}
