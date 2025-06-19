4074de0a9f56c1ee1590334636e1d64dff449206
Diya-Jaincoding <diyajain220362@acropolis.in>
2025-06-19 22:08:02 +0530

    Day 23: Linked List Creation and Traversal

---
+++ 23_linked_list_intro.cpp
@@ -0,0 +1,38 @@
+//1. Singly Linked List Basic Code
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+void printList(Node* head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    Node* head = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+
+    head->next = second;
+    second->next = third;
+
+    cout << "Linked List: ";
+    printList(head);
+
+    return 0;
+}
+
